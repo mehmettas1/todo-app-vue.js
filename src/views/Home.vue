b0d8faf4b877d94a17b93f64dcf19db0f1a2a1d7
mehmet <tas349873@gmail.com>
2023-09-03 14:17:02 +0300

    first-commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">TODO APP</div>
+</template>
+
+<script>
+
+
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      yapilacaklar:[]
+    }
+  },
+  mounted(){
+fetch('http://localhost:3000/yapilacaklar')
+.then((res)=>res.json())
+.then((data)=>this.yapilacaklar=data)
+.catch(())
+  }
+  ,
+};
+</script>
