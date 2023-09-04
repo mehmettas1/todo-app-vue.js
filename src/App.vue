5bf4eaf1c098d4ee714379dda2d25fcaa5cd2b75
mehmet <tas349873@gmail.com>
2023-09-04 22:46:06 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
+<script>
+import { Navbar } from "./components/Navbar.vue";
 
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 <style>
 body {
   background-color: #2c3e50;
