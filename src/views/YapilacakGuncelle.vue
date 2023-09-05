a23000ecaa0b02e65f5313d02d0897085777bc22
mehmet <tas349873@gmail.com>
2023-09-05 21:55:47 +0300

    update

---
+++ src/views/YapilacakGuncelle.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Yapılacak Güncelle</h1>
+  <form action="">
+    <label for="">Başlık:</label>
+    <input type="text" v-model="baslık" />
+    <label for="">icerik:</label>
+    <input type="text" v-model="icerik" />
+  </form>
+</template>
+<script>
+export default {};
+</script>
