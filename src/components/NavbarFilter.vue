123ec1c5f7bfe86fd6ab8fadd5ac84e8437190d6
mehmet <tas349873@gmail.com>
2023-09-05 22:22:50 +0300

    css

---
+++ src/components/NavbarFilter.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="filter-nav">
+    <button @click="filterGuncelle('hepsi')">Bütün Yaapılacaklar</button>
+    <button @click="filterGuncelle('Tamamlandı')">
+      Tamamlanan Yaapılacaklar
+    </button>
+    <button @click="filterGuncelle('yapilıyor')">
+      Devam eden Yapılacaklar
+    </button>
+  </nav>
+</template>
+<script>
+export default {
+  methods: {
+    filterGuncelle() {
+      this.$emit("filterDurum", durum);
+    },
+  },
+};
+</script>
+<style>
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+  color: #555;
+}
+</style>
